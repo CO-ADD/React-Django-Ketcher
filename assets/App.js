deea98d0407a9e75bfe0589980e3b437f5611e4c
Jo-Zh <joannaweinazh@gmail.com>
2023-03-26 12:14:39 +1000

    getsmiles

--- assets/App.js
+++ assets/App.js
@@ -1,38 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { StandaloneStructServiceProvider } from "ketcher-standalone";
-import { Editor, Ketcher } from "ketcher-react";
+import { Editor } from "ketcher-react";
 import "ketcher-react/dist/index.css";
-
-import Miew from "miew";
-
-window.Miew = Miew;
 
 const structServiceProvider = new StandaloneStructServiceProvider();
 
-export class KetcherExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleOnInit = this.handleOnInit.bind(this);
-  }
+function KetcherExample(props) {
+  const [ketcher, setKetcher] = useState(null);
+  const [smiles, setSmiles] = useState("");
 
-  handleOnInit = async (Ketcher) => {
-    this.ketcher = Ketcher;
-    window.ketcher = Ketcher;
-    console.log("djnago var = " + "{{molecular}}");
-    const mole = this.props.molecular.replace(/\\n/g, "\n");
+  const handleOnInit = (Ketcher) => {
+    setKetcher(Ketcher);
+    const mole = props.molecular.replace(/\\n/g, "\n");
     const initialData = mole;
-    this.ketcher.setMolecule(initialData);
+    Ketcher.setMolecule(initialData);
   };
 
-  render() {
-    return (
+  const getSmiles = () => {
+    ketcher.getSmiles().then((newSmiles) => {
+      console.log("SMILES:", newSmiles);
+      setSmiles(newSmiles);
+    });
+  };
+
+  return (
+    <div>
       <Editor
         staticResourcesUrl={""}
         structServiceProvider={structServiceProvider}
-        onInit={this.handleOnInit}
+        onInit={handleOnInit}
+        style={{ width: "400px", height: "400px" }}
       />
-    );
-  }
+      <button onClick={getSmiles}>Get SMILES</button>
+      <br />
+      <textarea
+        value={smiles}
+        readOnly={true}
+        id="getSmilesFromKetcher"
+        hidden
+      />
+    </div>
+  );
 }
 
 export default KetcherExample;
--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ import * as ReactDOMClient from "react-dom/client";
 import React from "react";
 import KetcherExample from "./App";
 
-const molecular = document.getElementById("mol").value.toString();
+const object_mol = document.getElementById("mol").value.toString();
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
-root.render(<KetcherExample molecular={molecular} />);
+root.render(<KetcherExample molecular={object_mol} />);
