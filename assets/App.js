1bc6213b2fa541eb1114b077619678b44fd0e364
Jo-Zh <joannaweinazh@gmail.com>
2023-05-13 20:16:46 +1000

    change to modal

--- assets/App.js
+++ assets/App.js
@@ -1,13 +1,18 @@
+import "./app.css";
 import React, { useState } from "react";
 import { StandaloneStructServiceProvider } from "ketcher-standalone";
 import { Editor } from "ketcher-react";
 import "ketcher-react/dist/index.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 const structServiceProvider = new StandaloneStructServiceProvider();
 
 function KetcherExample(props) {
   const [ketcher, setKetcher] = useState(null);
   const [smiles, setSmiles] = useState("");
+  const [show, setShow] = useState(false);
+  const [activeModalId, setActiveModalId] = useState(null);
 
   const handleOnInit = (Ketcher) => {
     setKetcher(Ketcher);
@@ -23,22 +28,58 @@ function KetcherExample(props) {
     });
   };
 
+  const handleShow = (id) => {
+    setActiveModalId(id);
+    setShow(true);
+  };
+
+  const handleClose = () => {
+    setActiveModalId(null);
+    setShow(false);
+  };
+
   return (
     <div>
-      <Editor
-        staticResourcesUrl={""}
-        structServiceProvider={structServiceProvider}
-        onInit={handleOnInit}
-        style={{ width: "400px", height: "400px" }}
-      />
-      <button onClick={getSmiles}>Get SMILES</button>
-      <br />
-      <textarea
-        value={smiles}
-        readOnly={true}
-        id="getSmilesFromKetcher"
-        hidden
-      />
+      <Button variant="primary" onClick={() => handleShow(props.drug_id)}>
+        {props.drug_id}
+      </Button>
+      <Modal
+        show={show && activeModalId === props.drug_id}
+        id={props.drug_id}
+        onHide={handleClose}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        dialogClassName="modal-90w"
+        // scrollable
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Editor
+            staticResourcesUrl={""}
+            structServiceProvider={structServiceProvider}
+            onInit={handleOnInit}
+            // style={{ width: "300px", height: "300px" }}
+          />
+          <button onClick={getSmiles}>Get SMILES</button>
+          <br />
+          <textarea
+            value={smiles}
+            readOnly={true}
+            id="getSmilesFromKetcher"
+            hidden
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- assets/index.js
+++ assets/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import KetcherExample from "./App";
 
 const object_mol = document.getElementById("mol").value.toString();
+const drug_id = "A001"; //document.getElementById("molecular_id").value.toString();
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
-root.render(<KetcherExample molecular={object_mol} />);
+root.render(<KetcherExample molecular={object_mol} drug_id={drug_id} />);
