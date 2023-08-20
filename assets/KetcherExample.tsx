f938c711213157d335ddb38dedc5a29ed569d27d
Jo-Zh <joannaweinazh@gmail.com>
2023-08-20 18:33:09 +1000

    update btn

--- assets/KetcherExample.tsx
+++ assets/KetcherExample.tsx
@@ -1,36 +1,57 @@
+import styled from "@emotion/styled";
 import "./app.css";
 import React, { useState } from "react";
 import { StandaloneStructServiceProvider } from "ketcher-standalone";
 import { Editor } from "ketcher-react";
 import "ketcher-react/dist/index.css";
-import CSS from 'csstype';
+import CSS from "csstype";
+import Viewer from "miew-react";
 
+// const MyComponent = () => {
+//   return <Viewer />;
+// };
+
+const GridWrapper = styled("div")`
+  height: 100vh;
+  width: 100vw;
+  max-height: 100vh;
+  max-width: 100vw;
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: 1fr 270px 320px;
+  grid-template-rows: 1fr;
+  gap: 0px 0px;
+  grid-template-areas: "Ketcher Panel Output";
+  & > div {
+    border: 1px solid grey;
+  }
+`;
 
 const structServiceProvider = new StandaloneStructServiceProvider();
-const editorstyle: CSS.Properties={
-  width: 'fit-content',
-  height:'fit-content',
-}
+const editorstyle: CSS.Properties = {
+  display: "flex",
+  flexDirection:"column",
+  width: "fit-content",
+  height: "fit-content",
+};
 //define prop and state type
-interface Props{
+interface Props {
   molecular: string;
   drug_id: string;
 }
-interface State{
+interface State {
   ketcher: any;
   smiles: string;
   show: boolean;
-  activeModalId: string | null; 
+  activeModalId: string | null;
 }
 
-
-
-function KetcherExample(props:Props) {
+function KetcherExample(props: Props) {
   const [ketcher, setKetcher] = useState<any>(null);
   const [sm_or_mo, setSm_or_Mo] = useState<string>("");
   // const [molfile, setMolfile] = useState<string>("");
-
-  const handleOnInit = (Ketcher:any) => {
+  const mol = props.molecular.replace(/\\n/g, "\n");
+  const handleOnInit = (Ketcher: any) => {
     setKetcher(Ketcher);
     const mole = props.molecular.replace(/\\n/g, "\n");
     const initialData = mole;
@@ -39,8 +60,7 @@ function KetcherExample(props:Props) {
 
   const getSmiles = () => {
     ketcher.getSmiles().then((newSmiles: string) => {
-      
-      localStorage.setItem('smiles_query', newSmiles);
+      localStorage.setItem("smiles_query", newSmiles);
       setSm_or_Mo(newSmiles);
     });
   };
@@ -54,36 +74,42 @@ function KetcherExample(props:Props) {
 
   // const updateMol = () => {
   //   ketcher.getSmiles().then((newSmiles: string) => {
-    
+
   //   });
   // };
 
-
-
   const errorHandler = (error: string) => {
     console.error(error);
-  }
+  };
 
   return (
+    // <div style={editorstyle}>
     <div style={editorstyle}>
-      {/* <button id="updateMol" onClick={updateMol}>Update Mol</button> */}
-      <button id="updateMol" >Update Mol</button>
+      <GridWrapper>
+        {/* <button id="updateMol" onClick={updateMol}>Update Mol</button> */}
+        <Editor
+          staticResourcesUrl={""}
+          structServiceProvider={structServiceProvider}
+          onInit={handleOnInit}
+          errorHandler={errorHandler}
+        />
+      </GridWrapper>
 
-          <Editor
-            staticResourcesUrl={""}
-            structServiceProvider={structServiceProvider}
-            onInit={handleOnInit}
-            errorHandler={errorHandler}
-           
-          />
-          <button onClick={getSmiles}>Get SMILES</button>
-          <button onClick={getMolfile}>Get Molfile</button>
-          <br />
-          <textarea
-            value={sm_or_mo}
-            readOnly={true}
-            id="getSmilesFromKetcher"
-          />
+      <div>
+
+        <button onClick={getSmiles}>Get SMILES</button>
+        <button onClick={getMolfile}>Get Molfile</button>
+        <br />
+        <button id="updateMol" disabled={true}>Update Mol</button>
+        <textarea
+          value={sm_or_mo}
+          readOnly={true}
+          id="getSmilesFromKetcher"
+          rows={5}
+          cols={10}
+        />
+      </div>
+      {/* </div> */}
     </div>
   );
 }
--- test_and_build.sh
+++ test_and_build.sh
@@ -2,26 +2,28 @@
 
 # Step 1: Run tests
 echo "Running tests..."
-python manage.py test 
-tests_exit_code=$?
+# python3 manage.py test 
+# tests_exit_code=$?
 
-if [ $tests_exit_code -ne 0 ]; then
-    echo "Tests failed. Aborting build process."
-    exit $tests_exit_code
-fi
+# if [ $tests_exit_code -ne 0 ]; then
+#     echo "Tests failed. Aborting build process."
+#     exit $tests_exit_code
+# fi
 
-# Step 2: Build index-bundle.js
-echo "Building index-bundle.js..."
-npm start dev
-build_exit_code=$?
+# # Step 2: Build index-bundle.js
+# echo "Building index-bundle.js..."
+# npm start build
+# build_exit_code=$?
 
-if [ $build_exit_code -ne 0 ]; then
-    echo "Build failed. Aborting build process."
-    exit $build_exit_code
-fi
+# if [ $build_exit_code -ne 0 ]; then
+#     echo "Build failed. Aborting build process."
+#     exit $build_exit_code
+# fi
 
 # Step 3: Copy index-bundle.js
 echo "Copying index-bundle.js to Main Project"
-cp index-bundle.js project/adjCOADD/static/js/ketcher/
+cp static/index-bundle.js ../adjCOADD/static/js/ketcher/
+# echo "Copying miew to adjCOADD."
+cp static/vendors-node_modules_miew-react_dist_index_modern_js.index-bundle.js ../adjCOADD/static/js/ketcher/
 
 echo "Build process completed successfully."
